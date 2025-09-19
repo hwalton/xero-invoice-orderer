ce02ec7686d0f0904b11f70c9ac4c312bdb94496
hwalton <harveywalton.hw@gmail.com>
2025-09-19 22:21:49 +0100

    unique owner tenant

--- migrations/000003_xero_connections.up.sql
+++ migrations/000003_xero_connections.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS xero_connections (
   id TEXT PRIMARY KEY,                -- UUID or generated id
-  owner_id TEXT NOT NULL,             -- your app's company/user id
+  owner_id TEXT NOT NULL UNIQUE,             -- your app's company/user id
   tenant_id TEXT NOT NULL,
   access_token TEXT NOT NULL,
   refresh_token TEXT NOT NULL,
